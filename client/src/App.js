721a9b6980dfd912c198035d02eaa2cc0e518638
kurtisp <kurtis.pace2011@gmail.com>
2019-05-01 16:46:34 -0600

    saving to get merged with the master

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GameLevel from "./components/GameLevel"
+import Paddle from "./components/Paddle"
 
 function App() {
   return (
-    <GameLevel></GameLevel>
+    <Paddle></Paddle>
   );
 }
 
