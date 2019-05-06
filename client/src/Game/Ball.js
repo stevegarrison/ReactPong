d1e43ee25d5f6a803712356db74a7e88a98963be
kurtisp <kurtis.pace2011@gmail.com>
2019-05-06 10:56:53 -0600

    fixed ball size bug

--- client/src/Game/Ball.js
+++ client/src/Game/Ball.js
@@ -97,9 +97,9 @@ class Ball {
         // _context.fillRect(this.m_positionX, this.m_positionY, this.m_width, this.m_height);
 
         _context.beginPath();
-        _context.ellipse(this.m_positionX, this.m_positionY, this.m_width, this.m_height, Math.PI * this.m_width, 0, Math.PI * this.m_width);
+        _context.ellipse(this.m_positionX, this.m_positionY, this.m_width/2, this.m_height/2, Math.PI * this.m_width, 0,  Math.PI * this.m_width);
         _context.fill();
-       // this.renderCollisionRect(_context);
+    //    this.renderCollisionRect(_context);
 
     }
 
