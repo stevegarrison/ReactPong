d448d45edf4ce708ce4e507b7bfee5a71c6f21a8
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-05-09 13:33:25 -0600

    Pull

---
+++ client/build/precache-manifest.c00325b45d31826028c53cbde8b9fef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "668fb028a21c9ac485b0",
+    "url": "/static/css/main.18fc1d39.chunk.css"
+  },
+  {
+    "revision": "668fb028a21c9ac485b0",
+    "url": "/static/js/main.6c0dc549.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9353c0797cdfdc4307d2",
+    "url": "/static/js/2.c207ee4b.chunk.js"
+  },
+  {
+    "revision": "3f712ddbb4d4932e1cd16570ef5c853b",
+    "url": "/static/media/BG-IMG.3f712ddb.png"
+  },
+  {
+    "revision": "c029d072a9031ed7492f25359615addb",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/options", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds123603.mlab.com:23603/heroku_ddfqjl91", { useNewUrlParser: true });
 
 // Every NPM start creates default options and DB
 db.options
