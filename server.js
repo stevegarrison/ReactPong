ffa0a0a5151e7b059472e8831c5b2fc960cfc56e
kurtisp <kurtis.pace2011@gmail.com>
2019-05-14 21:53:25 -0600

    fixed the canvas loading bug

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/options", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds123603.mlab.com:23603/heroku_5lxdp3bs", { useNewUrlParser: true });
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/options", { useNewUrlParser: true });
 
 // Every NPM start creates default options and DB
 db.options
